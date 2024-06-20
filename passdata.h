b25880795b80cf7d33e0e5e31a2c257dd572d0d2
mrquw <mrquwerty@outlook.com>
2024-06-20 13:08:36 +0800

    commit

--- passdata.h
+++ passdata.h
@@ -9,10 +9,31 @@
 
 using namespace std;
 
-struct PassData {
-    string username;
-    string pass;
-    string service;
-};
+string normalizeString(string p) {
+    return p + string(64 - p.size(), '/');
+}
+
+vector<uint32_t> stringToInt(string &s) {
+    vector<uint32_t> data (s.size() >> 2);
+    uint32_t *nPtr = reinterpret_cast<uint32_t*>(&*s.begin());
+
+    for (uint32_t &i: data) {
+        i = *nPtr;
+        nPtr++;
+    }
+    return data;
+}
+
+string intToString(const vector<uint32_t> &data) {
+    string s;
+    char* cPtr = (char*)(&*data.begin());
+
+    for (int i = 0; i < data.size() << 2; ++i) {
+        s += *cPtr;
+        cPtr++;
+    }
+
+    return s;
+}
 
 #endif //PPASS_PASSDATA_H
