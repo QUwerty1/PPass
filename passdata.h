af8b1e26ba9e0fbb95f28154dc88f5fe962e06b5
mrquw <mrquwerty@outlook.com>
2024-06-20 11:17:36 +0800

    created passdata.h and main.cpp converted to utf-8

---
+++ passdata.h
@@ -0,0 +1,18 @@
+//
+// Created by mrquw on 20.06.2024.
+//
+
+#ifndef PPASS_PASSDATA_H
+#define PPASS_PASSDATA_H
+
+#include <string>
+
+using namespace std;
+
+struct PassData {
+    string username;
+    string pass;
+    string service;
+};
+
+#endif //PPASS_PASSDATA_H
